7532c395853d99894b699c35b7ffd1ee3ef9a1ab
Derman01 <54042384+Derman01@users.noreply.github.com>
2020-04-24 02:17:45 +0500

    Update Готовые коды.cs

--- Готовые коды.cs
+++ Готовые коды.cs
@@ -63,3 +63,32 @@ static (string x1, string x2)[] Read_corteg(bool[,] R, string[] A)
                     }
             return corteg;
         }
+
+//Разбиение множества на классы эквивалентности
+static string[][] Ekv(string[] M, HashSet<(string x1,string x2)> corteg)
+        {
+            var B = new string[1][];
+            B[0] = new string[1];
+            foreach (var a in M)
+            {
+                var was = false;
+                for (int i = 0; i < B.Length; i++)
+                {
+                    for (int j = 0; j < B[i].Length; j++)
+                        if (corteg.Contains((B[i][j], a)))
+                        {
+                            Array.Resize(ref B[i], B[i].Length + 1);
+                            B[i][B[i].Length - 1] = a;
+                            was = true;
+                            break;
+                        }
+                }
+                if (!was)
+                {
+                    Array.Resize(ref B, B.Length + 1);
+                    Array.Resize(ref B[B.Length - 1], 1);
+                    B[B.Length - 1][0] = a;
+                }
+            }
+            return B;
+        }
